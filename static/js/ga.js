32ede07a511cecc5b4ce31fc846c07f83eb07529
blackmaker07 <blackmaker07@gmail.com>
2024-12-16 14:00:14 +0100

    rework analytics

--- static/js/ga.js
+++ static/js/ga.js
@@ -1,5 +1,3 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-R7E7T91HG8"></script>
-
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
