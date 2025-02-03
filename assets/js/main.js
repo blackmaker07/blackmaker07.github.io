295a4776aebb47ea4d25693045c3c5c0f76cfb74
blackmaker07 <blackmaker07@gmail.com>
2025-02-03 22:27:13 +0100

    code optimize

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,5 @@ console.log('Diese Seite wurde von TrueWords.at erstellt.');
 import * as bootstrap from 'bootstrap';
 window.bootstrap = bootstrap;
 
-document.addEventListener("DOMContentLoaded", function(event) {
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-});
-
